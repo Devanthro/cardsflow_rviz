1ddf08f3457f26eb6223751fe8685120fff52001
letrend <simon.trendel@tum.de>
2018-10-17 14:50:58 +0200

    with mesh and tendon visualization

--- include/cardsflow_rviz/cardsflow_rviz.hpp
+++ include/cardsflow_rviz/cardsflow_rviz.hpp
@@ -14,23 +14,37 @@
 #include <QScrollArea>
 #include <QListWidget>
 #include <QStyledItemDelegate>
+
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <tf_conversions/tf_eigen.h>
+
 #include <Eigen/Core>
 #include <Eigen/Dense>
+
 #include <rviz/panel.h>
 #include <pluginlib/class_loader.h>
 #include <pluginlib/class_list_macros.h>
+
+#include <common_utilities/rviz_visualization.hpp>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <roboy_communication_simulation/Tendon.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <sensor_msgs/JointState.h>
+
+#include <map>
+#include <thread>
+
 #endif
 
 using namespace std;
 using namespace Eigen;
 
 
-class CardsflowRviz : public rviz::Panel {
+class CardsflowRviz : public rviz::Panel, public rviz_visualization {
     Q_OBJECT
 
 public:
@@ -55,15 +69,60 @@ public:
 public Q_SLOTS:
 
     /**
-     * Connects to sensor_port and IP given via GUI
+     * Toggles mesh visualization
      */
     void show_mesh();
+
+    /**
+     * Toggles tendon visualization
+     */
+    void show_tendon();
+
+    /**
+     * Toggles force visualization
+     */
+    void show_force();
+
+    /**
+     * Toggles torque visualization
+     */
+    void show_torque();
+
+private:
+    /**
+     * Callback to robot state messages
+     * @param msg
+     */
+    void RobotState(const geometry_msgs::PoseStampedConstPtr &msg);
+    /**
+     * Callback for Tendon state messages
+     * @param msg
+     */
+    void TendonState(const roboy_communication_simulation::TendonConstPtr &msg);
+
+    /**
+     * Visualization loop, run in a detached thread
+     */
+    void visualizationLoop();
 
 private:
     ros::NodeHandlePtr nh;
     boost::shared_ptr<ros::AsyncSpinner> spinner;
-    ros::Publisher visualization_pub;
-    ros::Subscriber pose_correction_sub;
+    ros::Subscriber robot_state, tendon_state;
     tf::TransformListener tf_listener;
     tf::TransformBroadcaster tf_broadcaster;
+    ros::Publisher robot_state_pub, joint_state_pub;
+    map<string, geometry_msgs::Pose> pose;
+    struct Tendon{
+        float force;
+        float l;
+        float ld;
+        vector<geometry_msgs::Vector3> viaPoints;
+    };
+    map<string, Tendon> tendon;
+    bool visualize_mesh, visualize_tendon, visualize_force, visualize_torque;
+    boost::shared_ptr<std::thread> visualization_thread;
+    QPushButton *show_mesh_button, *show_tendon_button, *show_force_button, *show_torque_button;
+    string robot_name;
+    bool shutdown = false;
 };
--- src/cardsflow_rviz.cpp
+++ src/cardsflow_rviz.cpp
@@ -18,9 +18,25 @@ CardsflowRviz::CardsflowRviz(QWidget *parent)
     connectWidget->setLayout(new QVBoxLayout);
     tabPage->addTab(connectWidget, "connect");
 
-    QPushButton *sm = new QPushButton(tr("show_mesh"));
-    connect(sm, SIGNAL(clicked()), this, SLOT(show_mesh()));
-    connectWidget->layout()->addWidget(sm);
+    show_mesh_button = new QPushButton(tr("show_mesh"));
+    show_mesh_button->setCheckable(true);
+    connect(show_mesh_button, SIGNAL(clicked()), this, SLOT(show_mesh()));
+    connectWidget->layout()->addWidget(show_mesh_button);
+
+    show_tendon_button = new QPushButton(tr("show_tendon"));
+    show_tendon_button->setCheckable(true);
+    connect(show_tendon_button, SIGNAL(clicked()), this, SLOT(show_tendon()));
+    connectWidget->layout()->addWidget(show_tendon_button);
+
+    show_force_button = new QPushButton(tr("show_force"));
+    show_force_button->setCheckable(true);
+    connect(show_force_button, SIGNAL(clicked()), this, SLOT(show_force()));
+    connectWidget->layout()->addWidget(show_force_button);
+
+    show_torque_button = new QPushButton(tr("show_torque"));
+    show_torque_button->setCheckable(true);
+    connect(show_torque_button, SIGNAL(clicked()), this, SLOT(show_torque()));
+    connectWidget->layout()->addWidget(show_torque_button);
 
     frameLayout->addWidget(tabPage);
 
@@ -47,29 +63,120 @@ CardsflowRviz::CardsflowRviz(QWidget *parent)
 
     spinner = boost::shared_ptr<ros::AsyncSpinner>(new ros::AsyncSpinner(0));
 
+    robot_state = nh->subscribe("/robot_state", 1, &CardsflowRviz::RobotState, this);
+    tendon_state = nh->subscribe("/tendon_state", 1, &CardsflowRviz::TendonState, this);
+
+    if (!nh->hasParam("robot_name"))
+        ROS_FATAL("robot_name could not be found on parameter server!!! ");
+    else
+        nh->getParam("robot_name", robot_name);
+
 }
 
 CardsflowRviz::~CardsflowRviz() {
-
-}
-
-void CardsflowRviz::save(rviz::Config config) const {
-//        QLineEdit *w = this->findChild<QLineEdit *>("IP");
-//        config.mapSetValue(w->objectName(), w->text());
-//        w = this->findChild<QLineEdit *>("client_port");
-//        config.mapSetValue(w->objectName(), w->text());
-//        rviz::Panel::save(config);
+    shutdown = true;
+    ROS_INFO("waiting for visualization thread to terminate");
+    if (visualization_thread->joinable())
+        visualization_thread->join();
 }
 
 void CardsflowRviz::load(const rviz::Config &config) {
-//        rviz::Panel::load(config);
-//        QLineEdit *w = this->findChild<QLineEdit *>("IP");
-//        QVariant text;
-//        config.mapGetValue(w->objectName(), &text);
-//        w->setText(text.toString());
-//        w = this->findChild<QLineEdit *>("client_port");
-//        config.mapGetValue(w->objectName(), &text);
-//        w->setText(text.toString());
+    rviz::Panel::load(config);
+    QVariant ticked;
+    config.mapGetValue(show_mesh_button->objectName(), &ticked);
+    show_mesh_button->setChecked(ticked.toBool());
+    config.mapGetValue(show_tendon_button->objectName(), &ticked);
+    show_tendon_button->setChecked(ticked.toBool());
+    config.mapGetValue(show_force_button->objectName(), &ticked);
+    show_force_button->setChecked(ticked.toBool());
+    config.mapGetValue(show_torque_button->objectName(), &ticked);
+    show_torque_button->setChecked(ticked.toBool());
+    visualization_thread = boost::shared_ptr<std::thread>(new std::thread(&CardsflowRviz::visualizationLoop, this));
+    visualization_thread->detach();
+    show_mesh();
+    show_tendon();
+    show_force();
+    show_torque();
+}
+
+void CardsflowRviz::save(rviz::Config config) const {
+    config.mapSetValue(show_mesh_button->objectName(), show_mesh_button->isChecked());
+    config.mapSetValue(show_tendon_button->objectName(), show_tendon_button->isChecked());
+    config.mapSetValue(show_force_button->objectName(), show_force_button->isChecked());
+    config.mapSetValue(show_torque_button->objectName(), show_torque_button->isChecked());
+    rviz::Panel::save(config);
+}
+
+void CardsflowRviz::show_mesh() {
+    visualize_mesh = show_mesh_button->isChecked();
+}
+
+void CardsflowRviz::show_tendon() {
+    visualize_tendon = show_tendon_button->isChecked();
+}
+
+void CardsflowRviz::show_force() {
+    visualize_force = show_force_button->isChecked();
+}
+
+void CardsflowRviz::show_torque() {
+    visualize_torque = show_torque_button->isChecked();
+}
+
+void CardsflowRviz::RobotState(const geometry_msgs::PoseStampedConstPtr &msg) {
+    pose[msg->header.frame_id] = msg->pose;
+}
+
+void CardsflowRviz::TendonState(const roboy_communication_simulation::TendonConstPtr &msg) {
+    Tendon t;
+    t.force = msg->force;
+    t.l = msg->l;
+    t.ld = msg->ld;
+    t.viaPoints = msg->viaPoints;
+    tendon[msg->name] = t;
+}
+
+void CardsflowRviz::visualizationLoop() {
+    ros::Rate r(30);
+    while (ros::ok() && !shutdown) {
+        int message_id = 0;
+        if (visualize_mesh) {
+            for (auto p:pose) {
+                publishMesh("robots", (robot_name + "/meshes/CAD").c_str(), (p.first + ".stl").c_str(), p.second, 0.001,
+                            "world", "mesh", message_id++, 1);
+            }
+        }
+        if (visualize_tendon) {
+            visualization_msgs::Marker line_strip;
+            line_strip.header.frame_id = "world";
+            line_strip.header.stamp = ros::Time::now();
+            line_strip.ns = "tendon";
+            line_strip.action = visualization_msgs::Marker::ADD;
+            line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+            line_strip.scale.x = 0.003;
+            line_strip.color.b = 1.0;
+            line_strip.color.a = 1.0;
+            line_strip.pose.orientation.w = 1.0;
+            line_strip.lifetime = ros::Duration(1);
+            for (auto t:tendon) {
+                line_strip.points.clear();
+                line_strip.id = message_id++;
+                for (int i = 1; i < t.second.viaPoints.size(); i++) {
+                    geometry_msgs::Point p;
+                    p.x = t.second.viaPoints[i - 1].x;
+                    p.y = t.second.viaPoints[i - 1].y;
+                    p.z = t.second.viaPoints[i - 1].z;
+                    line_strip.points.push_back(p);
+                    p.x = t.second.viaPoints[i].x;
+                    p.y = t.second.viaPoints[i].y;
+                    p.z = t.second.viaPoints[i].z;
+                    line_strip.points.push_back(p);
+                    visualization_pub.publish(line_strip);
+                }
+            }
+        }
+        r.sleep();
+    }
 }
 
 PLUGINLIB_EXPORT_CLASS(CardsflowRviz, rviz::Panel)
